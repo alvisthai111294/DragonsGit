7503dacc4a09c76f5e0f434cd268c6fbf5152a99
Nick the Great <Nick the Great@NICKS-PC>
2020-05-03 17:52:10 -0700

    Add sub method for testing class

---
+++ HomeApp/src/myGUI/homeGUITest.java
@@ -0,0 +1,99 @@
+package myGUI;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class homeGUITest {
+
+	@Test
+	void testHomeGUI() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMainFrame() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAboutButton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearchTextField() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearchButton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUserProfileButton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPhotoButton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testYouOwnTextField() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testYesCheckBox() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testNoCheckBox() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddToolBoxButton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLinkToMenus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGeneralNotes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGeneralLabel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testInitialize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetVersion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetVersion() {
+		fail("Not yet implemented");
+	}
+
+}
